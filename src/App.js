c0624d5f90c91e1fc679c704a246749c91d5145c
Sehrish88 <sehrishbalouch4@gmail.com>
2020-09-25 18:44:22 -0400

    create aApp and TimeOfDay component

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TimeOfDay from './TimeOfDay';
+
+
+const App = () => {
+ return (  
+
+    <div className="box">
+        <TimeOfDay />
+
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+    </div>
+ )
+}
+
+export default App 
